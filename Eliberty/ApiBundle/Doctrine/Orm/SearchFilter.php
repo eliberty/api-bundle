6c1e8096e139f5bb32d15eebc12af0bf7a3dc216
Xavier L <xlembo@eliberty.fr>
2015-08-19 21:19:36 +0200

    correction searchfilters

--- Eliberty/ApiBundle/Doctrine/Orm/SearchFilter.php
+++ Eliberty/ApiBundle/Doctrine/Orm/SearchFilter.php
@@ -15,6 +15,7 @@ use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ORM\QueryBuilder;
 use Dunglas\ApiBundle\Api\IriConverterInterface;
 use Dunglas\ApiBundle\Api\ResourceInterface;
+use Dunglas\ApiBundle\Doctrine\Orm\Filter\AbstractFilter;
 use Dunglas\ApiBundle\Doctrine\Orm\Filter\SearchFilter as BaseSearchFilter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
@@ -24,13 +25,146 @@ use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
  *
  * @author philippe Vesin <pvesin@eliberty.fr>
  */
-class SearchFilter extends BaseSearchFilter
+class SearchFilter extends AbstractFilter
 {
+    /**
+     * @var string Exact matching.
+     */
+    const STRATEGY_EXACT = 'exact';
+    /**
+     * @var string The value must be contained in the field.
+     */
+    const STRATEGY_PARTIAL = 'partial';
+
+    /**
+     * @var IriConverterInterface
+     */
+    private $iriConverter;
+    /**
+     * @var PropertyAccessorInterface
+     */
+    private $propertyAccessor;
+
+    /**
+     * @param ManagerRegistry           $managerRegistry
+     * @param IriConverterInterface     $iriConverter
+     * @param PropertyAccessorInterface $propertyAccessor
+     * @param null|array                $properties       Null to allow filtering on all properties with the exact strategy or a map of property name with strategy.
+     */
+    public function __construct(
+        ManagerRegistry $managerRegistry,
+        IriConverterInterface $iriConverter,
+        PropertyAccessorInterface $propertyAccessor,
+        array $properties = null
+    ) {
+        parent::__construct($managerRegistry, $properties);
+
+        $this->iriConverter = $iriConverter;
+        $this->propertyAccessor = $propertyAccessor;
+    }
+
+
     /**
      * @return array|null
      */
     public function getProperties()
     {
         return $this->properties;
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function apply(ResourceInterface $resource, QueryBuilder $queryBuilder, Request $request)
+    {
+        $metadata = $this->getClassMetadata($resource);
+        $fieldNames = array_flip($metadata->getFieldNames());
+
+        foreach ($this->extractProperties($request) as $property => $value) {
+            if (!is_string($value) || !$this->isPropertyEnabled($property)) {
+                continue;
+            }
+
+            $partial = null !== $this->properties && self::STRATEGY_PARTIAL === $this->properties[$property];
+            $propertyValue = $partial ? sprintf('%%%s%%', $value) : $value;
+
+            if (isset($fieldNames[$property])) {
+                if ('id' === $property) {
+                    $value = $this->getFilterValueFromUrl($value);
+                }
+
+                $propertyType = $metadata->getTypeOfField($property);
+                $equalityString = $partial ? 'LOWER(o.%1$s) LIKE :%1$s' : 'o.%1$s = :%1$s';
+
+                $queryBuilder
+                    ->andWhere(sprintf($equalityString, $property))
+                    ->setParameter($property, $propertyValue)
+                ;
+            } elseif ($metadata->isSingleValuedAssociation($property)
+                || $metadata->isCollectionValuedAssociation($property)
+            ) {
+                $value = $this->getFilterValueFromUrl($value);
+
+                $queryBuilder
+                    ->join(sprintf('o.%s', $property), $property)
+                    ->andWhere(sprintf('%1$s.id = :%1$s', $property))
+                    ->setParameter($property, $propertyValue)
+                ;
+            }
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDescription(ResourceInterface $resource)
+    {
+        $description = [];
+        $metadata = $this->getClassMetadata($resource);
+
+        foreach ($metadata->getFieldNames() as $fieldName) {
+            $found = isset($this->properties[$fieldName]);
+            if ($found || null === $this->properties) {
+                $description[$fieldName] = [
+                    'property' => $fieldName,
+                    'type' => $metadata->getTypeOfField($fieldName),
+                    'required' => false,
+                    'strategy' => $found ? $this->properties[$fieldName] : self::STRATEGY_EXACT,
+                ];
+            }
+        }
+
+        foreach ($metadata->getAssociationNames() as $associationName) {
+            if ($this->isPropertyEnabled($associationName)) {
+                $description[$associationName] = [
+                    'property' => $associationName,
+                    'type' => 'iri',
+                    'required' => false,
+                    'strategy' => self::STRATEGY_EXACT,
+                ];
+            }
+        }
+
+        return $description;
+    }
+
+    /**
+     * Gets the ID from an URI or a raw ID.
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    private function getFilterValueFromUrl($value)
+    {
+        try {
+            if ($item = $this->iriConverter->getItemFromIri($value)) {
+                return $this->propertyAccessor->getValue($item, 'id');
+            }
+        } catch (\InvalidArgumentException $e) {
+            // Do nothing, return the raw value
+        }
+
+        return $value;
     }
 }
