36af4677eedf280811178627a40bb3e4494b5a46
Alain Louage <alain.louage@eliberty.fr>
2020-01-29 16:58:22 +0100

    PR feedback

--- Eliberty/ApiBundle/Api/Resource.php
+++ Eliberty/ApiBundle/Api/Resource.php
@@ -571,7 +571,7 @@ class Resource implements ResourceInterface
         $objIdentity = new ObjectIdentity('class', $entityClass);
 
         if (!$this->authorizationChecker->isGranted($mask, $objIdentity)) {
-            throw new AccessDeniedException(sprintf('%s: Acl permission for this object is not granted for %s.',$entityClass, \implode($mask, '')));
+            throw new AccessDeniedException(sprintf('%s: Acl permission for this object is not granted for %s.',$entityClass, \implode('', $mask)));
         }
     }
 
